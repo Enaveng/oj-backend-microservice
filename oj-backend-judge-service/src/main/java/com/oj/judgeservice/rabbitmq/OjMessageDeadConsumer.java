652d49f2f226c362ac9a56451eb7432ea6323eef
吴涛 <wutao@andwyf.com>
2024-01-31 18:10:21 +0800

    优化代码

--- oj-backend-judge-service/src/main/java/com/oj/judgeservice/rabbitmq/OjMessageDeadConsumer.java
+++ oj-backend-judge-service/src/main/java/com/oj/judgeservice/rabbitmq/OjMessageDeadConsumer.java
@@ -1,6 +1,6 @@
 package com.oj.judgeservice.rabbitmq;
 
-import cn.hutool.json.JSONUtil;
+import com.oj.client.service.QuestionServiceFeignClient;
 import com.oj.common.common.ErrorCode;
 import com.oj.common.config.RabbitMqConfig;
 import com.oj.common.exception.BusinessException;
@@ -14,6 +14,7 @@ import org.springframework.amqp.support.AmqpHeaders;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.Resource;
 import java.io.IOException;
 
 
@@ -24,6 +25,9 @@ import java.io.IOException;
 @Component
 public class OjMessageDeadConsumer {
 
+    @Resource
+    private QuestionServiceFeignClient questionServiceFeignClient;
+
 
     @RabbitListener(queues = RabbitMqConfig.OJ_DEAD_QUEUE_NAME)
     public void receiveDeadMessage(String message, Channel channel, @Header(AmqpHeaders.DELIVERY_TAG) long deliveryTag) throws IOException {
@@ -33,11 +37,14 @@ public class OjMessageDeadConsumer {
             throw new BusinessException(ErrorCode.PARAMS_ERROR, "消息为空");
         }
         long questionSubmitId = Long.parseLong(message);
-        //修改题目状态为失败
+        //修改题目提交状态为失败
         QuestionSubmit questionSubmitUpdate = new QuestionSubmit();
         questionSubmitUpdate.setId(questionSubmitId);
-        //将JudgeInfo对象转换为json对象
         questionSubmitUpdate.setStatus(QuestionSubmitStatusEnum.FAILED.getValue());
+        boolean update = questionServiceFeignClient.updateQuestionSubmitById(questionSubmitUpdate);
+        if (!update) {
+            throw new BusinessException(ErrorCode.SYSTEM_ERROR, "题目状态更新错误");
+        }
         // 确认消息
         channel.basicAck(deliveryTag, false);
     }
